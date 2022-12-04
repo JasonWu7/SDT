1d13678b2b19ea76b9ed42c9222f6783a40e8ab0
Dongzheng Wu <dwuu0018@student.monash.edu>
2022-12-04 11:06:26 +1100

    Fixed typo, added instruction

--- src/setting.js
+++ src/setting.js
@@ -21,8 +21,11 @@ function Setting(props) {
             localStorage.setItem(e.key, address.current.value);
             alert(`Successfully Set Key ${e.char}`);
         }
-        
     }
+ }
+
+ function openInstruction(){
+    window.open("https://github.com/JasonWu7/smalldesktop");
  }
 
   return (
@@ -40,11 +43,12 @@ function Setting(props) {
     ></img>
     <br/><br/>
         {setting && <div className='Setting-panel'>
-            <button onClick={props.handleBg}>Change Backgroud Image</button><hr/>
+            <button onClick={props.handleBg}>Change Background Image</button><hr/>
             <button onClick={props.handleNt}>Add Notes</button><hr/>
             <button onClick={function () {
             setsc(prev => {return !prev})
         }}>Set Shortcut</button><hr/>
+            <button onClick={openInstruction}>Instruction</button><hr/>
             </div>}
             {sc && <div className='Shortcut-setting'>
                 <input placeholder='Enter Key' ref = {key}/><br/>
